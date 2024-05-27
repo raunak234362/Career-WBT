dd5c7122d83693b0befe28b152936afdf873e994
raunak234362 <raunakdob@gmail.com>
2024-05-27 20:17:19 +0530

    chore: Update student profile component layout and display student data

--- src/components/student/Profile.jsx
+++ src/components/student/Profile.jsx
@@ -30,43 +30,47 @@ const Profile = () => {
 
   return (
     <div className="max-full mx-auto p-6 bg-white rounded-md shadow-md">
-      <h1 className="text-3xl flex font-bold mb-6 items-center justify-center text-center">Profile</h1>
-      <div className="mb-4">
-        <h2 className="text-xl font-semibold">Student Name</h2>
-        <p className="text-gray-700">{formData?.name}</p>
-      </div>
-      <div className="mb-4">
-        <h2 className="text-xl font-semibold">Father Name</h2>
-        <p className="text-gray-700">{formData?.fatherName}</p>
-      </div>
-      <div className="mb-4">
-        <h2 className="text-xl font-semibold">Mother Name</h2>
-        <p className="text-gray-700">{formData?.motherName}</p>
-      </div>
-      <div className="mb-4">
-        <h2 className="text-xl font-semibold">College ID</h2>
-        <p className="text-gray-700">{formData?.studentId}</p>
-      </div>
-      <div className="mb-4">
-        <h2 className="text-xl font-semibold">Email</h2>
-        <p className="text-gray-700">{formData?.email}</p>
-      </div>
-      <div className="mb-4">
-        <h2 className="text-xl font-semibold">Contact Number</h2>
-        <p className="text-gray-700">{formData?.phone}</p>
-      </div>
-      <div className="mb-4">
-        <h2 className="text-xl font-semibold">Final Semester</h2>
-        <p className="text-gray-700">{formData?.currentSemester}</p>
-      </div>
-      <div className="mb-4">
-        <h2 className="text-xl font-semibold">Marks/CGPA</h2>
-        <p className="text-gray-700">{formData?.cgpa}</p>
-      </div>
-      <div className="mb-4">
-        <h2 className="text-xl font-semibold">Backlogs</h2>
-        <p className="text-gray-700">{formData?.backlog}</p>
-      </div>
+      <h1 className="text-3xl font-bold mb-6 text-center">Profile</h1>
+      {formData && (
+        <div>
+          <div className="mb-4">
+            <h2 className="text-xl font-semibold">Student Name</h2>
+            <p className="text-gray-700">{formData.name}</p>
+          </div>
+          <div className="mb-4">
+            <h2 className="text-xl font-semibold">Father Name</h2>
+            <p className="text-gray-700">{formData.fatherName}</p>
+          </div>
+          <div className="mb-4">
+            <h2 className="text-xl font-semibold">Mother Name</h2>
+            <p className="text-gray-700">{formData.motherName}</p>
+          </div>
+          <div className="mb-4">
+            <h2 className="text-xl font-semibold">College ID</h2>
+            <p className="text-gray-700">{formData.studentId}</p>
+          </div>
+          <div className="mb-4">
+            <h2 className="text-xl font-semibold">Email</h2>
+            <p className="text-gray-700">{formData.email}</p>
+          </div>
+          <div className="mb-4">
+            <h2 className="text-xl font-semibold">Contact Number</h2>
+            <p className="text-gray-700">{formData.phone}</p>
+          </div>
+          <div className="mb-4">
+            <h2 className="text-xl font-semibold">Final Semester</h2>
+            <p className="text-gray-700">{formData.currentSemester}</p>
+          </div>
+          <div className="mb-4">
+            <h2 className="text-xl font-semibold">Marks/CGPA</h2>
+            <p className="text-gray-700">{formData.cgpa}</p>
+          </div>
+          <div className="mb-4">
+            <h2 className="text-xl font-semibold">Backlogs</h2>
+            <p className="text-gray-700">{formData.backlog}</p>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
