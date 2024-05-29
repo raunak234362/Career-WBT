8b84f0ed06c26643eab4111fd65ea624b3ddf391
raunak234362 <raunakdob@gmail.com>
2024-05-29 19:42:55 +0530

    refactor: Update StudentProfile component to include margin in the container

--- src/components/student/StudentProfile.jsx
+++ src/components/student/StudentProfile.jsx
@@ -40,43 +40,53 @@ const StudentProfile = () => {
   return (
     <div className="max-full mx-auto p-6 m-5 bg-white rounded-md shadow-md">
       <h1 className="text-3xl flex font-bold mb-6 items-center justify-center text-center">Profile</h1>
-     
+      <div className="flex flex-row justify-between">
+
+      </div>
+          {/* <div><img src={formData?.profile} alt="image" /></div>
+          <div className='flex justify-center mb-6'>
+            <img
+              src={`https://wbt-quizcave.onrender.com/${formData?.profile}`}
+              alt='Profile Pic'
+              className='w-32 h-32 rounded-full object-cover'
+            />
+          </div> */}
         <>
-          <div className="mb-4">
+          <div className="mb-4 flex flex-row items-center">
             <h2 className="text-xl font-semibold">Student Name</h2>
-            <p className="text-gray-700">{formData?.name}</p>
+            <p className="text-gray-700 text-xl px-5">{formData?.name}</p>
           </div>
-          <div className="mb-4">
+          <div className="mb-4 flex flex-row items-center">
             <h2 className="text-xl font-semibold">Father Name</h2>
-            <p className="text-gray-700">{formData?.fatherName}</p>
+            <p className="text-gray-700 text-xl px-5">{formData?.fatherName}</p>
           </div>
-          <div className="mb-4">
+          <div className="mb-4 flex flex-row items-center">
             <h2 className="text-xl font-semibold">Mother Name</h2>
-            <p className="text-gray-700">{formData?.motherName}</p>
+            <p className="text-gray-700 text-xl px-5">{formData?.motherName}</p>
           </div>
-          <div className="mb-4">
+          <div className="mb-4 flex flex-row items-center">
             <h2 className="text-xl font-semibold">College ID</h2>
-            <p className="text-gray-700">{formData?.studentId}</p>
+            <p className="text-gray-700 text-xl px-5">{formData?.studentId}</p>
           </div>
-          <div className="mb-4">
+          <div className="mb-4 flex flex-row items-center">
             <h2 className="text-xl font-semibold">Email</h2>
-            <p className="text-gray-700">{formData?.email}</p>
+            <p className="text-gray-700 text-xl px-5">{formData?.email}</p>
           </div>
-          <div className="mb-4">
+          <div className="mb-4 flex flex-row items-center">
             <h2 className="text-xl font-semibold">Contact Number</h2>
-            <p className="text-gray-700">{formData?.phone}</p>
+            <p className="text-gray-700 text-xl px-5">{formData?.phone}</p>
           </div>
-          <div className="mb-4">
+          <div className="mb-4 flex flex-row items-center">
             <h2 className="text-xl font-semibold">Final Semester</h2>
-            <p className="text-gray-700">{formData?.currentSemester}</p>
+            <p className="text-gray-700 text-xl px-5">{formData?.currentSemester}</p>
           </div>
-          <div className="mb-4">
+          <div className="mb-4 flex flex-row items-center">
             <h2 className="text-xl font-semibold">Marks/CGPA</h2>
-            <p className="text-gray-700">{formData?.cgpa}</p>
+            <p className="text-gray-700 text-xl px-5">{formData?.cgpa}</p>
           </div>
-          <div className="mb-4">
+          <div className="mb-4 flex flex-row items-center">
             <h2 className="text-xl font-semibold">Backlogs</h2>
-            <p className="text-gray-700">{formData?.backlog}</p>
+            <p className="text-gray-700 text-xl px-5">{formData?.backlog}</p>
           </div>
         </>
  
