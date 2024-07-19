d58dbb4be8cdb82e7175ad48626fe9d8c3114c7b
raunak234362 <raunakdob@gmail.com>
2024-07-19 12:57:08 +0530

    refactor: Update image URLs in components
    The image URLs in the Header, Question, AdminProfile, PdfCreator, and StudentProfile components have been updated to use the IMG_URL constant instead of the BASE_URL constant. This change ensures consistency and improves maintainability by centralizing the image URL configuration. It also aligns with the recent update to the BASE_URL constant.

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
 // export const BASE_URL="https://wbt-quizcave.onrender.com"
 // export const BASE_URL="http://49.205.192.113:8000"
-export const BASE_URL="http://192.168.1.154:8000"
-export const IMG_URL="http://192.168.1.154:5555"
+export const BASE_URL="http://192.168.1.215:8000"
+export const IMG_URL="http://192.168.1.215:5555"
