1bcaa5685371f85f5caa3732d27e2b69f128b660
raunak234362 <raunakdob@gmail.com>
2024-11-06 18:54:13 +0530

    refactor: Update BASE_URL and IMG_URL in constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
 // export const BASE_URL="https://wbt-quizcave.onrender.com"
 // export const BASE_URL="http://49.205.192.113:8000"
-export const BASE_URL="https://192.168.1.215:808"
+export const BASE_URL="https://192.168.1.215:8000"
 export const IMG_URL="https://192.168.1.215:808"
