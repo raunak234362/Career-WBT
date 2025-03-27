8e45043556c5e794b49d432f67290962153577e2
Jhanavi B M <115522962+JhanaviBM@users.noreply.github.com>
2025-03-27 12:34:37 +0530

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
 // export const BASE_URL="https://wbt-quizcave.onrender.com"
 // export const BASE_URL="http://49.205.192.113:8000"
-export const BASE_URL = "https://localhost:8080";
+export const BASE_URL = "https://192.168.1.157:8080";
 export const IMG_URL="http://106.51.141.125:5555"
