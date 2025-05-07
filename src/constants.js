e43093efebb6125c02ca8cd966d5b5ff4286b7b1
Jhanavi <jhanavigowda03@gmail.com>
2025-05-07 11:23:49 +0530

    Update BASE_URL in constants.js to new development server address

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,5 @@
 // export const BASE_URL="https://wbt-quizcave.onrender.com"
-// export const BASE_URL="http://49.205.192.113:8000"
-export const BASE_URL = "https://192.168.1.157:8080";
+// export const BASE_URL="https://quizcavebe.whiteboardtec.com:8080"
+// export const BASE_URL = "https://192.168.1.157:8080";
 export const IMG_URL="http://106.51.141.125:5555"
+export const BASE_URL = "http://192.168.1.26:8080";
